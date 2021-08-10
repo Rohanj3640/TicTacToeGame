93d7c86d052eebcfe1bbd39f37ef4e6a309a388c
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-10 09:17:14 -0700

    Modified: code for board layout

--- src/com/BridgeLabz/TicTacToe.java
+++ src/com/BridgeLabz/TicTacToe.java
@@ -2,6 +2,21 @@ package com.BridgeLabz;
 
 public class TicTacToe {
     public static void main(String[] args) {
+               //Printing welcome message
         System.out.println("Welcome to Tic Tac Toe Game");
+             //calling method to initialise the board
+        createBoard();
+    }
+          //Created a method createBoard and also defined array named "i" of size 1
+         //Initializing the index of array starting from 1 to assign the empty space
+         //Creating method for empty board
+        //0th index is ignored
+
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for(int index = 0; index < board.length; index++) {
+            board[index] = ' ';
+        }
+        return board;
     }
 }
